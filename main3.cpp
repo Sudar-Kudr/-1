302884711bf1c6d313f0c5ab81eec0c7700e09cd
Sudar-Kudr <70635302+Sudar-Kudr@users.noreply.github.com>
2020-09-09 17:16:32 +0300

    Add files via upload

---
+++ main3.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  ЛР1 зад3
+//
+//  Created by User on 09.09.2020.
+//  Copyright © 2020 User. All rights reserved.
+//
+/*
+     33333
+    33   33
+        33
+      333
+        33
+         33
+    333333
+*/
+// ЗАДАЧА 3 /////////////////////////////////////////////////
+#include <iostream>
+#include <cmath>
+#include <ios>
+
+int main()
+{
+    bool m, n, k, l;
+    std::cout << "Здравствуйте!\nВведите m=";
+    std::cin>>m;
+    std::cout << "Введите n=";
+    std::cin>>n;
+    std::cout << "Введите k=";
+    std::cin>>k;
+    std::cout << "Введите l=";
+    std::cin>>l;
+    std::cout << std::boolalpha << ((m||n)&&(!k&&!l)) << "\n" ;  
+    /*
+    int Otv=((m||n)&&(!k&&!l));
+    std::cout << "Ответ: ";
+    if (Otv == 1)
+    {
+        std::cout << "Истина\n";
+    }
+    else
+    {
+        std::cout << "Ложь\n";
+    }
+    */
+    return 0;
+}
