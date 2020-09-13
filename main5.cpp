a1d6b9170072cb5b1abc609d008f4a130f77cc4c
Sudar-Kudr <70635302+Sudar-Kudr@users.noreply.github.com>
2020-09-13 21:15:04 +0300

    Add files via upload

---
+++ main5.cpp
@@ -0,0 +1,21 @@
+// ЗАДАЧА 5 /////////////////////////////////////////////////
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    unsigned short int e, f, d, c;
+    unsigned int Otv;
+    std::cout << "Здравствуйте!\nВведите e=";
+    std::cin>>e;
+    std::cout << "Введите f=";
+    std::cin>>f;
+    std::cout << "Введите d=";
+    std::cin>>d;
+    std::cout << "Введите c=";
+    std::cin>>c;
+    Otv=(e&(((~f)&d)|(f&(~d))))|(~c);
+    std::cout << std::hex << Otv << "\n";
+
+    return 0;
+}
